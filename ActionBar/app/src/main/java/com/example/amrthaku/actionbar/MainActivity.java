68d2c6586bb5861375a1f199fd08e810b66d7ddf
unknown <amrthaku@DIN16001215.corp.capgemini.com>
2018-01-22 16:30:52 +0530

    first commit

---
+++ ActionBar/app/src/main/java/com/example/amrthaku/actionbar/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.amrthaku.actionbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.add:
+            //add the function to perform here
+            textView = (TextView) findViewById(R.id.textView);
+            textView.setText("Add");
+            return(true);
+        case R.id.reset:
+            textView = (TextView) findViewById(R.id.textView);
+            textView.setText("Reset");
+            return(true);
+        case R.id.about:
+            Toast.makeText(this, R.string.about_toast, Toast.LENGTH_LONG).show();
+            return(true);
+        case R.id.exit:
+            finish();
+            return(true);
+    }
+        return(super.onOptionsItemSelected(item));
+    }
+}
